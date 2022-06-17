2b462b6286d3ef0b4062aee6ebce39d160cf1142
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-06-17 20:58:53 -0300

    criado as funções de criar uma nova playlist e mostrar na tela todas as playlists

--- modulo1/labefy/src/App.js
+++ modulo1/labefy/src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [inputNewPlaylist, setInputNewPlaylist] = useState('');
+  const [playlist, setPlaylist] = useState([]);
+  const handleInputNewPlaylist = e => {
+    setInputNewPlaylist(e.target.value);
+  };
+
+  const createPlaylist = () => {
+    axios
+      .post(
+        'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+        {
+          name: inputNewPlaylist
+        },
+        { headers: { Authorization: 'nicole-peiker-franklin' } }
+      )
+      .then(response => {
+        console.log(response);
+        alert(`A playlist ${inputNewPlaylist} foi criada com sucesso`);
+        getAllPlaylist();
+        setInputNewPlaylist('');
+      })
+      .catch(error => console.log(error.message));
+  };
+
+  const getAllPlaylist = () => {
+    axios
+      .get(
+        'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+        { headers: { Authorization: 'nicole-peiker-franklin' } }
+      )
+      .then(response => {
+        setPlaylist(response.data.result.list);
+      })
+      .catch(error => console.log(error.message));
+  };
+
+  useEffect(getAllPlaylist, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          onChange={handleInputNewPlaylist}
+          value={inputNewPlaylist}
+          placeholder="Nome da Lista"
+        />
+        <button onClick={createPlaylist}>Criar playlist</button>
+      </div>
+      <div>
+        <h3>Minhas playlists</h3>
+        {playlist.map(playlist => {
+          return <li key={playlist.id}>{playlist.name}</li>;
+        })}
+      </div>
     </div>
   );
 }
