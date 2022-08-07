97efa1e22faba116e0bbfb315feda6a2852cb936
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-08-07 17:31:52 -0300

    initial commit

---
+++ modulo3/Labebank/src/index.ts
@@ -0,0 +1,17 @@
+export class Client{
+  constructor(
+    name:string, 
+    cpf:number, 
+    birthDate:string){} 
+}
+
+export class CreateAccount{
+
+}
+
+export class Account{
+  constructor(client:Client, balance:number)
+  {}
+  
+}
+
