be6ccfa7bbcebad7503e03b3782945c48926f347
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-08-23 08:29:25 -0300

    organização dos arquivos e alteração do endpoint de ciração do usuário utilizando o knex

--- modulo3/to-do-list/src/index.ts
+++ modulo3/to-do-list/src/index.ts
@@ -1,32 +1,75 @@
-import express from 'express'
-import cors from 'cors'
-import { AddressInfo } from "net";
-import knex from "knex";
+import app from "./app";
+import express, {Request, Response} from 'express'
+import connection from "./connection";
 import dotenv from "dotenv";
 
-const app = express( ) 
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+
+
+app.post("/user", async(req: Request, res:Response) => {
+  let errorStatus = 500
+  try {
+    const {name, nickname, email} = req.body
+    if(!name || !nickname || !email){
+      errorStatus = 400
+      throw new Error("É necessário preencher todos os dados para se cadastrar")
+    }
+    await connection("TodoListUser")
+    .insert({
+      id: Date.now().toString(),
+      name:req.body.name,
+      nickname:req.body.nickname,
+      email: req.body.email, 
+
+    }).into("TodoListUser")
+    res.status(200).send('Usuário criado com sucesso.')
+    //res.status(400).send('É necessario preencher todas as informações para criar a conta.')
+    
+  } catch (error: any) {
+    console.log(error);
+    res.status(errorStatus).send(error.message)
   }
-});
 
-app.use(express.json( )) 
-app.use(cors( )) 
+})
 
-dotenv.config();
-
-const server = app.listen(process.env.PORT || 3003, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server is running in http://localhost:${address.port}`);
-  } else {
-    console.error(`Failure upon starting server.`);
+app.get("/user/all", async(req: Request, res: Response) => {
+  let errorStatus = 500
+  try {
+    const result = await connection("TodoListUser")   
+    .select("id", "nickname") 
+    .from("TodoListUser")
+    .where({id: req.params.id})
+    if(result.length === 0){
+      errorStatus = 400
+      throw new Error("Usuário não encontrado")
+    }
+    res.status(200).send(result)
+  } catch (error) {
+    console.log(error);
+    res.status(errorStatus).send(error.message)
   }
-});
+})
+
+app.get("/user/:id", (req: Request, res:Response) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+
+app.put("/user/edit/:id", (req, res)=>{
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+
+app.post("/task", (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+})
 
