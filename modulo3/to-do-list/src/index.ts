76dfdab9440f846f92cd5e3cb21aa554f44d783b
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-08-23 11:03:52 -0300

    criado pasta de endpoints

--- modulo3/to-do-list/src/index.ts
+++ modulo3/to-do-list/src/index.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import express, {Request, Response} from 'express'
 import connection from "./connection";
 import dotenv from "dotenv";
-
+import {getUserById} from './endpoints/getUserbyId';
 
 
 app.post("/user", async(req: Request, res:Response) => {
@@ -16,8 +16,8 @@ app.post("/user", async(req: Request, res:Response) => {
     await connection("TodoListUser")
     .insert({
       id: Date.now().toString(),
-      name:req.body.name,
-      nickname:req.body.nickname,
+      name: req.body.name,
+      nickname: req.body.nickname,
       email: req.body.email, 
 
     }).into("TodoListUser")
@@ -32,30 +32,7 @@ app.post("/user", async(req: Request, res:Response) => {
 })
 
 app.get("/user/all", async(req: Request, res: Response) => {
-  let errorStatus = 500
-  try {
-    const result = await connection("TodoListUser")   
-    .select("id", "nickname") 
-    .from("TodoListUser")
-    .where({id: req.params.id})
-    if(result.length === 0){
-      errorStatus = 400
-      throw new Error("Usuário não encontrado")
-    }
-    res.status(200).send(result)
-  } catch (error) {
-    console.log(error);
-    res.status(errorStatus).send(error.message)
-  }
-})
-
-app.get("/user/:id", (req: Request, res:Response) => {
-  try {
-    
-  } catch (error) {
-    
-  }
-})
+  })
 
 app.put("/user/edit/:id", (req, res)=>{
   try {
