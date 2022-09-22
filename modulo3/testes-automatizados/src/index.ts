ac7a89191c0e4142f5dbc22d303cafa82cde23c6
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-09-22 08:28:19 -0300

    exercicio 3 não finalizado

--- modulo3/testes-automatizados/src/index.ts
+++ modulo3/testes-automatizados/src/index.ts
@@ -1,4 +1,5 @@
 import { User } from './models/exercicio1';
+import { Casino, IResultItemOutputDTO, IResultOutputDTO, NACIONALITY, UserCasino } from './models/exercicio3';
 
 export function performPurchase(user: User, value: number): User | undefined {
   if (user.balance >= value) {
@@ -9,3 +10,37 @@ export function performPurchase(user: User, value: number): User | undefined {
     return undefined;
   }
 }
+
+
+function verifyAge(casino: Casino, users: UserCasino[]): IResultOutputDTO {
+  const allowedBR: {}[] = []
+  const unallowedBR: {}[] = []
+  const allowedUS: {}[] = []
+  const unallowedUS: {}[] = []
+
+  for(const user of users) {
+    if(user.age >= 18 && user.nacionality === NACIONALITY.BRAZILIAN){
+      allowedBR.push(user)
+    } if(user.age >= 18 && user.nacionality === NACIONALITY.AMERICAN){
+      allowedUS.push(user)
+    } if(user.age < 18 && user.nacionality === NACIONALITY.BRAZILIAN){
+      unallowedBR.push(user)
+    } if(user.age < 18 && user.nacionality === NACIONALITY.AMERICAN){
+      unallowedUS.push(user)
+    } 
+
+  for(const allowed of alloweds){}
+
+  const result  = {
+    brazilians: { 
+      allowed: allowedBR,
+      notAllowed: unallowedBR
+    },
+    americans: { 
+      allowed: allowedUS,
+      notAllowed: unallowedUS
+    }
+  } 
+  return result
+}
+
--- modulo3/testes-automatizados/src/models/exercicio1.ts
+++ modulo3/testes-automatizados/src/models/exercicio1.ts
@@ -2,12 +2,3 @@ export interface User {
   name: string;
   balance: number;
 }
-
-enum LOCATION {
-  EUA = 'EUA',
-  BRASIL = 'BRASIL'
-}
-
-enum NACIONALITY {
-  AMERICAN = ''
-}
